e766af8281174243e03550238326283e2eab4653
Harshal Sharma <harshal.sharma@synapse.com>
2020-01-12 21:38:07 +0530

    Added basic repository and controller to search Game titles.

--- src/main/java/com/example/basicspringapplication/GameController.java
+++ src/main/java/com/example/basicspringapplication/GameController.java
@@ -1,14 +1,30 @@
 package com.example.basicspringapplication;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.basicspringapplication.entities.Game;
+import com.example.basicspringapplication.repositories.GameRepository;
 
 @RestController
 public class GameController {
 
+	@Autowired
+	GameRepository repository;
+	
 	@GetMapping("/hello")
 	public String basicController() {
 		return "Hello World!";
+	}
+	
+	
+	@GetMapping("/searchGameByTitle")
+	public List<Game> searchGames(@RequestParam("title") String title){
+		return repository.findByTitleContaining(title);
 	}
 	
 }
